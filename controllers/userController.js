889a5418c1025be4ef8932ff19a90f0c862c8d6c
JC-Coder <josephchimezie2003@gmail.com>
2022-12-19 23:22:57 +0100

    changed confirmpassword reqired to false to enable updating user model without error, added addressdetails schema , added update address route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,92 +1,158 @@
-const User = require('./../models/userModel');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
-const { getAll, getOne, updateOne, deleteOne, geSuspended, getActive, getUnverified, getStudents, getTutors } = require('./handleFactory');
-const { Suspended, UnSuspended, Approved, Declined } = require('../utils/verifyEmail')
+const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+const {
+  getAll,
+  getOne,
+  updateOne,
+  deleteOne,
+  geSuspended,
+  getActive,
+  getUnverified,
+  getStudents,
+  getTutors,
+} = require("./handleFactory");
+const {
+  Suspended,
+  UnSuspended,
+  Approved,
+  Declined,
+} = require("../utils/verifyEmail");
+const AddressDetailsModel = require("./../models/AddressDetailsModel");
 
 const filterObj = (obj, ...allowedFields) => {
-    const newObj = {};
-    Object.keys(obj).forEach(el => {
-        if (allowedFields.includes(el)) newObj[el] = obj[el];
-    });
-    return newObj;
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
 };
 exports.getMe = (req, res, next) => {
-    req.params.id = req.user.id;
-    next();
+  req.params.id = req.user.id;
+  next();
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-    if (req.body.password || req.body.passwordConfirm) {
-        return next(
-            new AppError('This route is not for password updates. Please use /updateMyPassword.', 400)
-        );
-    }
-    const filteredBody = filterObj(req.body, 'firstName', 'lastName', 'phoneNumber', 'email', 'photo');
-    if (req.file) filteredBody.photo = req.file.filename;
-    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true });
-    res.status(200).json({
-        status: 'success',
-        data: {
-            data: updatedUser
-        }
-    });
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use /updateMyPassword.",
+        400
+      )
+    );
+  }
+  const filteredBody = filterObj(
+    req.body,
+    "firstName",
+    "lastName",
+    "phoneNumber",
+    "email",
+    "photo"
+  );
+  if (req.file) filteredBody.photo = req.file.filename;
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: updatedUser,
+    },
+  });
 });
 
-exports.deleteMe = catchAsync(async (req, res, next) => {
-    await User.findByIdAndUpdate(req.user.id, { active: false });
-    res.status(201).json({
-        status: 'success',
-        data: null
+// update address details
+exports.updateAddressDetails = async (req, res) => {
+  const { id } = req.params;
+  const { province, city, postalCode, surBurb, street, complex } = req.body;
+
+  try {
+    const user = await User.findById(id).populate("addressDetails");
+    console.log(user);
+    if (!user) {
+      res.status(404).json("No user found with id");
+    }
+
+    const addressExist = await AddressDetailsModel.findOne({
+      userId: user._id,
     });
+    console.log(addressExist);
+
+    if (!addressExist) {
+      const newAddressDetails = await new AddressDetailsModel({
+        ...req.body,
+        userId: user._id,
+      }).save();
+      user.addressDetails = newAddressDetails;
+      await user.save();
+      console.log("not exist so save");
+
+      res.status(201).json({ message: "Address updated successfully" , user});
+    } else {
+      console.log("exists ");
+
+      res.status(201).json({ message: "Address updated successfully" });
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+  res.status(201).json({
+    status: "success",
+    data: null,
+  });
 });
 
 exports.susPendUser = catchAsync(async (req, res, next) => {
-    const data = await User.findByIdAndUpdate(req.params.id, { status: false });
-    if (!data) return next(new AppError("No user with this Id", 400));
-    const Email = data.email
-    const Name = data.firstName
-    Suspended(Email, Name)
-    res.status(201).json({
-        status: 'success',
-        data: data
-    });
+  const data = await User.findByIdAndUpdate(req.params.id, { status: false });
+  if (!data) return next(new AppError("No user with this Id", 400));
+  const Email = data.email;
+  const Name = data.firstName;
+  Suspended(Email, Name);
+  res.status(201).json({
+    status: "success",
+    data: data,
+  });
 });
 
 exports.unSusPendUser = catchAsync(async (req, res, next) => {
-    const data = await User.findByIdAndUpdate(req.params.id, { status: true });
-    if (!data) return next(new AppError("No user with this Id", 400));
-    const Email = data.email
-    const Name = data.firstName
-    UnSuspended(Email, Name)
-    res.status(201).json({
-        status: 'success',
-        data: data
-    });
+  const data = await User.findByIdAndUpdate(req.params.id, { status: true });
+  if (!data) return next(new AppError("No user with this Id", 400));
+  const Email = data.email;
+  const Name = data.firstName;
+  UnSuspended(Email, Name);
+  res.status(201).json({
+    status: "success",
+    data: data,
+  });
 });
 
 exports.ApprovedTutor = catchAsync(async (req, res, next) => {
-    const data = await User.findByIdAndUpdate(req.params.id, { verified: true });
-    if (!data) return next(new AppError("No user with this Id", 400));
-    const Email = data.email
-    const Name = data.firstName
-    Approved(Email, Name)
-    res.status(201).json({
-        status: 'success',
-        data: data
-    });
+  const data = await User.findByIdAndUpdate(req.params.id, { verified: true });
+  if (!data) return next(new AppError("No user with this Id", 400));
+  const Email = data.email;
+  const Name = data.firstName;
+  Approved(Email, Name);
+  res.status(201).json({
+    status: "success",
+    data: data,
+  });
 });
 
 exports.DeclineTutor = catchAsync(async (req, res, next) => {
-    const data = await User.findByIdAndUpdate(req.params.id, { verified: false });
-    if (!data) return next(new AppError("No user with this Id", 400));
-    const Email = data.email
-    const Name = data.firstName
-    Declined(Email, Name)
-    res.status(201).json({
-        status: 'success',
-        data: data
-    });
+  const data = await User.findByIdAndUpdate(req.params.id, { verified: false });
+  if (!data) return next(new AppError("No user with this Id", 400));
+  const Email = data.email;
+  const Name = data.firstName;
+  Declined(Email, Name);
+  res.status(201).json({
+    status: "success",
+    data: data,
+  });
 });
 
 exports.getAllUsers = getAll(User);
@@ -96,5 +162,5 @@ exports.deleteUser = deleteOne(User);
 exports.getSusPendUser = geSuspended(User, false);
 exports.getNonActive = getActive(User, false);
 exports.getUnVerify = getUnverified(User, false);
-exports.geTutorsRouteFunc = getTutors(User, 'tutor');
-exports.getStudentRouteFunc = getStudents(User, 'student');
+exports.geTutorsRouteFunc = getTutors(User, "tutor");
+exports.getStudentRouteFunc = getStudents(User, "student");
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,31 +1,53 @@
-const express = require('express');
-const { userSignUp, login, forgotPassword, resetPassword, protect, updatePassword, restrictTo, logout, verify } = require('./../controllers/authController');
-const { getAllUsers, updateMe, getUser, deleteUser, getMe, susPendUser, unSusPendUser, ApprovedTutor, DeclineTutor } = require('../controllers/userController');
-const { uploadUserPhoto, resizeUserPhoto } = require('../controllers/imageController')
+const express = require("express");
+const {
+  userSignUp,
+  login,
+  forgotPassword,
+  resetPassword,
+  protect,
+  updatePassword,
+  restrictTo,
+  logout,
+  verify,
+} = require("./../controllers/authController");
+const {
+  getAllUsers,
+  updateMe,
+  getUser,
+  deleteUser,
+  getMe,
+  susPendUser,
+  unSusPendUser,
+  ApprovedTutor,
+  DeclineTutor,
+  updateAddressDetails,
+} = require("../controllers/userController");
+const {
+  uploadUserPhoto,
+  resizeUserPhoto,
+} = require("../controllers/imageController");
 
 const router = express.Router({ mergeParams: true });
 
-router.post('/user/signUp', userSignUp);
+router.post("/user/signUp", userSignUp);
 
-router.post('/login', login);
-router.post('/logout', protect, logout);
-router.post('/verify/:id/:token/', verify);
-router.post('/forgotPassword', forgotPassword);
-router.patch('/resetPassword/:token', resetPassword);
-router.get('/me', protect, getMe, getUser)
-router.patch('/updateMyPassword', protect, updatePassword);
-router.patch('/updateMe', protect, uploadUserPhoto, resizeUserPhoto, updateMe);
+router.post("/login", login);
+router.post("/logout", protect, logout);
+router.post("/verify/:id/:token/", verify);
+router.post("/forgotPassword", forgotPassword);
+router.patch("/resetPassword/:token", resetPassword);
+router.get("/me", protect, getMe, getUser);
+router.patch("/updateMyPassword", protect, updatePassword);
+router.patch("/updateMe", protect, uploadUserPhoto, resizeUserPhoto, updateMe);
 
+router.route("/").get(protect, restrictTo("super-admin"), getAllUsers);
+router.route("/:id").get(protect, getUser).delete(protect, deleteUser);
 
-router.route('/').get(protect, restrictTo('super-admin'), getAllUsers)
-router
-    .route('/:id')
-    .get(protect, getUser)
-    .delete(protect, deleteUser)
+router.post("/:id/suspended", protect, susPendUser);
+router.post("/:id/un-suspended", protect, unSusPendUser);
+router.post("/:id/approved", protect, ApprovedTutor);
+router.post("/:id/decline", protect, DeclineTutor);
 
-router.post('/:id/suspended', protect, susPendUser);
-router.post('/:id/un-suspended', protect, unSusPendUser);
-router.post('/:id/approved', protect, ApprovedTutor);
-router.post('/:id/decline', protect, DeclineTutor);
+router.post("/:id/update-address", updateAddressDetails);
 
-module.exports = router
+module.exports = router;
