980824658692075767b9127998c9369641911f74
Chadrack kyungu <65617666+chadrackkyungu@users.noreply.github.com>
2022-12-20 15:35:41 +0200

    joseph changes

---
+++ .history/controllers/productController_20221219212115.js
@@ -0,0 +1,261 @@
+/* eslint-disable prettier/prettier */
+const Tour = require('../models/tourModel');
+
+//* Note this file here you use it in a controller only if you want to apply features on the API, features like [sort, pagination, limit, ect...]
+// const APIFeatures = require('../utils/apiFeatures');
+
+//* Note:  In all your controllers you will always need this 2 files, one to throw a catch error, and the other one to throw a request error(if the user made a wrong request)
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const { deleteOne, updateOne, createOne, getOne, getAll } = require('../controllers/handleFactory');
+
+exports.aliasTopTours = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
+}
+
+//* This is a middleware that check if the params user [ID] exist in the list of all my tour and then execute, if it does not exist then return this result
+exports.checkID = (req, res, next, val) => {
+    const { id } = req.params;
+    if (!id) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid ID",
+        });
+    }
+    next();
+};
+
+//* This middleware check if the request that the user made does not contain name and price, than it return an Error(this middleware get executed)
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: "fail",
+            message: "The Price or the Name is missing",
+        });
+    }
+    next();
+};
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el];
+    });
+    return newObj;
+};
+
+exports.getAllTours = getAll(Tour)
+//* I'm passing this to populate the reviews
+exports.getParams = getOne(Tour, { path: 'reviews' })
+exports.updateTour = updateOne(Tour)
+exports.createTour = createOne(Tour)
+exports.deleteTour = deleteOne(Tour)
+
+
+// exports.createTour = catchAsync(async (req, res, next) => {
+//     const tour = await Tour.create(req.body);
+
+//     if (!tour) {
+//         return next(new AppError('No tour found with that ID', 404));
+//     }
+
+//     res.status(201).json({
+//         status: "success",
+//         requestedAt: req.requestTime,
+//         data: {
+//             data: tour
+//         },
+//     });
+// });
+
+
+// exports.updateTour = catchAsync(async (req, res, next) => {
+
+//     const filteredBody = filterObj(req.body,
+//         'startDates',
+//         'name', 'gender', 'price', 'duration',
+//         'maxGroupSize', 'difficulty', 'priceDiscount', 'summary', 'description',
+//         'imageCover', 'images'
+//     );
+
+//     if (req.file) filteredBody.imageCover = req.file.filename;
+//     if (req.file) filteredBody.images = req.file.filename;
+
+//     const updatedTour = await Tour.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true });
+
+//     res.status(200).json({
+//         status: 'success',
+//         data: {
+//             data: updatedTour
+//         }
+//     });
+// });
+
+
+
+//* AGGREGATION [pipeline] ==> you can find this inside MongoDB Documentations
+exports.getTourStats = catchAsync(async (req, res, next) => {
+    const tour = await Tour.aggregate([
+        {
+            $match: { ratingsAverage: { $gte: 4.5 } }
+        },
+        {
+            //* Why? Pipeline, i use pipeline to get the number of tour that have similar [ratings], [min price], [max price], ect... 
+            //* if 3 tour has same price, or same ratings, so it will return that to me.
+            $group: {
+                // _id: '$ratingsAverage',
+                _id: '$difficulty', // this will return to me the number of tour that has [difficult] [medium], & [easy]
+                numTours: { $sum: 1 },
+                numRatings: { $sum: '$ratingsQuantity' }, //* This will calculate the number of ratings of all the tour
+                avgRating: { $avg: '$ratingsAverage' }, //* This will calculate the avg rating of all the tour
+                avgPrice: { $avg: '$price' }, //* the avg price of all the tour
+                minPrice: { $min: '$price' }, //* the min price of all the tour
+                maxPrice: { $max: '$price' } //* the max price of all the tour
+            }
+        },
+        {
+            $sort: { avgPrice: 1 } //* am sorting the field call <avgPrice> 
+        }
+    ]);
+    res.status(200).json({
+        status: "success",
+        requestedAt: req.requestTime,
+        data: {
+            tour
+        }
+    });
+});
+
+//*Note; to see more of this go to mongoDB documentation go under Reference => Server => Reference => Operators => query
+//* AGGREGATION [unwind] 
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+    const year = req.params.year * 1; // 2022
+    const tour = await Tour.aggregate([
+        {
+            //* [unwind] help you filter the tours that are running on a specific date 
+            $unwind: '$startDates'
+        },
+        {
+            //* here i'm matching
+            $match: {
+                startDates: {
+                    $gte: new Date(`${year}-01-01`),
+                    $lte: new Date(`${year}-12-31`)
+                }
+            }
+        },
+        {
+            //* here i'm returning the result to the user
+            $group: {
+                _id: { $month: '$startDates' }, //*  the month of when is the going to happen 
+                numTourStarts: { $sum: 1 }, //* the number of tours that are happening in that month
+                tours: { $push: '$name' } //* return an array of all tour names that are happening in the specified month
+            }
+        },
+        {
+            $addFields: { month: '$_id' }
+        },
+        { //* [Project] removes this =>  _id: { $month: '$startDates' } from my tour response
+            $project: {
+                _id: 0
+            }
+        },
+        { //* [Sort] from the bigger month to the smallest one
+            $sort: { numTourStarts: -1 }
+        },
+        {//* [limit] only 12 month
+            $limit: 12
+        }
+    ]);
+
+    res.status(200).json({
+        status: 'success',
+        requestedAt: req.requestTime,
+        results: tour.length,
+        data: {
+            tour
+        }
+    });
+});
+
+
+
+
+//* Geo Location 
+// /tours-within/:distance/center/:latlng/unit/:unit
+// /tours-within/233/center/34.111745,-118.113491/unit/mi
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+
+    //* this below are my params 
+    const { distance, latlng, unit } = req.params;
+    const [lat, lng] = latlng.split(',');
+
+    const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+    if (!lat || !lng) {
+        next(
+            new AppError(
+                'Please provide latitutr and longitude in the format lat,lng.',
+                400
+            )
+        );
+    }
+
+    const tours = await Tour.find({
+        startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+    });
+
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            data: tours
+        }
+    });
+});
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+    const { latlng, unit } = req.params;
+    const [lat, lng] = latlng.split(',');
+
+    const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+    if (!lat || !lng) {
+        next(
+            new AppError(
+                'Please provide latitutr and longitude in the format lat,lng.',
+                400
+            )
+        );
+    }
+
+    const distances = await Tour.aggregate([
+        {
+            $geoNear: {
+                near: {
+                    type: 'Point',
+                    coordinates: [lng * 1, lat * 1]
+                },
+                distanceField: 'distance',
+                distanceMultiplier: multiplier
+            }
+        },
+        {
+            $project: {
+                distance: 1,
+                name: 1
+            }
+        }
+    ]);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            data: distances
+        }
+    });
+});
---
+++ .history/routes/productRoutes_20221219221103.js
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+const express = require('express');
+const { getProductById, checkID, updateProduct, getAllProducts, createProduct } = require('../controllers/productController');
+const { uploadTourImages, resizeTourImages } = require('../controllers/imageController');
+// const reviewRouter = require('../routes/reviewRoutes');
+// const bookingRouter = require('../routes/bookingRoutes');
+const { protect, restrictTo } = require('../controllers/authController');
+
+const router = express.Router();
+router.param('id', checkID);
+
+//* Creating a review on a tour
+//? These 2 routes will merge they are params, one into reviewRouter.js & the other into bookingRouter.js
+// router.use('/:tourId/reviews', reviewRouter)  //* merge routes
+// router.use('/:tourId/bookings', bookingRouter)//* merge routes
+
+// router.route('/top-5-cheap').get(controllers.aliasTopTours, controllers.getAllTours);
+// router.route('/tour-stats').get(controllers.getTourStats);
+// router.route('/monthly-plan/:year').get(protect, restrictTo('admin', 'lead-guide', 'guide'), controllers.getMonthlyPlan);
+
+//* get tour by Id
+router.route('/:id/:x?/:y?').get(getProductById);
+router.patch('/:id').get(protect, updateProduct);
+
+router
+    .route('/')
+    .get(getAllProducts)
+    .post(protect, createProduct);
+
+router
+    .route('/:id')
+    .patch(protect, updateProduct)
+    .delete(protect, deleteProduct);
+
+module.exports = router;
