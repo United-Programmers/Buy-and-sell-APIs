6b218549586833bc42c31918dc58dc62e4830bdb
JC-Coder <josephchimezie2003@gmail.com>
2023-01-03 07:52:27 +0100

    added role to jwt signing on login

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,14 +12,14 @@ const {
   ApproveLecturer,
 } = require("../utils/verifyEmail");
 
-const signToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+const signToken = (id, role) => {
+  return jwt.sign({ id , role }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
 
 const createSendToken = (data, statusCode, res) => {
-  const token = signToken(data._id);
+  const token = signToken(data._id, data.role);
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
