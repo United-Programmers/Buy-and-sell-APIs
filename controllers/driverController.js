b1c727d3bd8f3fd37cecbf63ceda8bf4a98ba39f
JC-Coder <josephchimezie2003@gmail.com>
2023-01-14 00:22:38 +0100

    added update order status

--- controllers/driverController.js
+++ controllers/driverController.js
@@ -1,20 +1,48 @@
 const OrderModel = require("../models/orderModel");
+const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
 /**
  * get all assigned orders
  */
 exports.getAllAssignedOrders = catchAsync(async (req, res, next) => {
-    let driverId = req.user._id;
+  let driverId = req.user._id;
 
-    console.log(driverId);
+  console.log(driverId);
 
-    let assignedOrders = await OrderModel.find({
-        assignedTo: driverId
-    })
+  let assignedOrders = await OrderModel.find({
+    assignedTo: driverId,
+  });
 
-    return res.status(200).json({
-        status: "success",
-        data: assignedOrders
-    })
-})
+  return res.status(200).json({
+    status: "success",
+    data: assignedOrders,
+  });
+});
+
+/**
+ * update order status
+ */
+exports.updateOrderStatus = catchAsync(async (req, res) => {
+  let { orderId, answer, reason, orderUniqueId } = req.body;
+  let userId = req.user.id;
+
+  console.log(userId)
+
+  let order = await OrderModel.findById(orderId);
+  if (!order) {
+    throw new AppError("no order found with provided id", 404);
+  }
+
+  if(order.assignedTo !== userId){
+    throw new AppError("you are not authorized to perform this action", 401);
+  }
+
+  order.status = status;
+  let result = await order.save();
+
+  return res.status(200).json({
+    status: "success",
+    data: result,
+  });
+});
--- routes/driverRoutes.js
+++ routes/driverRoutes.js
@@ -1,8 +1,9 @@
 const { protect } = require('../controllers/authController');
-const { getAllAssignedOrders } = require('../controllers/driverController');
+const { getAllAssignedOrders, updateOrderStatus } = require('../controllers/driverController');
 
 const router = require('express').Router()
 
 router.get('/orders',protect, getAllAssignedOrders)
+router.patch('/order/status', updateOrderStatus)
 
 module.exports = router;
